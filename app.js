8dbc5c2e6824e928738400921d9bafadccaef0c0
kevmaillard <kemaillard@gmail.com>
2021-09-13 13:35:35 +0200

    second commit

--- app.js
+++ app.js
@@ -1,38 +1,20 @@
 console.log("Hello World :)");
                                                                                          // variables + variables NODEJS declarées
 const fs = require('fs');
-const request = require('request')
+var request = require('request')
 let jsontest = require('./json_files_eurotunnel/00086714.json');
 var headers = {
-  'Origin': 'http://fiddle.jshell.net',
-  'Accept-Encoding': 'gzip, deflate',
-  'Accept-Language': 'en-US,en;q=0.8',
-  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
-  'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-  'Accept': '*/*',
-  'Referer': 'http://fiddle.jshell.net/_display/',
-  'X-Requested-With': 'XMLHttpRequest',
-  'Connection': 'keep-alive'
+  'accept': '*/*',
+  'Content-Type': 'text/json'
 };
-var options = {
-  url: 'http://localhost:5000/api/v1/ITAccount',
-  method: 'POST',
-  headers: headers,
-  body: jsonOutput
-};
-
-
 
                                                                                         // extraction des données et transformation en objet JS
 var obj = {
   matricule: jsontest.PERNR,
-  numInfo: jsontest.ObjectSID,
-  etat: jsontest.Current_Step.Name
+  itAccountID: jsontest.ObjectSID,
+  currentStep: jsontest.Current_Step.Name
 };
 console.log(obj);
-
-// var testParse = JSON.parse(array);
-// console.log(testParse);
 
                                                                                         // création d'un format JSON à partir de l'objet JS
 var jsonOutput = JSON.stringify(obj);
@@ -48,14 +30,31 @@ fs.writeFile('output00086714.json', jsonOutput, 'utf8', function (err) {
     console.log("JSON file has been saved.");
 });
 
-                                                                                      // Envoi du JSON sur l'API
-
+                                                                                      // Envoi du JSON sur l'API >>> ne fonctionne pas encore
+// fetch('http://localhost:5000/api/v1/ITAccount', {
+//     method: 'POST',
+//     headers: {
+//         'accept': '*/*',
+//         'Content-Type': 'application/*+json'
+//     },
+//     body: JSON.stringify(obj)
+// });
+var options = {
+  url: 'http://localhost:5000/api/v1/ITAccount',
+  method: 'POST',
+  headers: headers,
+  body: jsonOutput
+};
 
 function callback(error, response, body) {
   if (!error && response.statusCode == 200) {
       console.log(body);
+  }else{
+    console.log("error");
   }
-
 }
 
 request(options, callback);
+
+
+
---
+++ readme.md
@@ -0,0 +1,6 @@
+Techno utilisée : JS / Node JS
+>> Récupere les infos du JSON
+>> Transforme les données en objet JS
+>> Formate l'objet en format JSON
+>> Crée un fichier JSON
+>> Envoi le fichier JSON sur l'API >>> Ne focntionne pas (encore)
